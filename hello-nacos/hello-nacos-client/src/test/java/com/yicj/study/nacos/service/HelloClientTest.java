65998fd4c91c20f5e9fec7faad1eb48f1bff71e3
yichengjie <626659321@qq.com>
2023-03-15 22:33:51 +0800

    save

---
+++ hello-nacos/hello-nacos-client/src/test/java/com/yicj/study/nacos/service/HelloClientTest.java
@@ -0,0 +1,18 @@
+package com.yicj.study.nacos.service;
+
+import com.yicj.study.nacos.NacosClientApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = NacosClientApplication.class)
+public class HelloClientTest {
+
+    @Test
+    public void addConfig(){
+
+
+    }
+}
