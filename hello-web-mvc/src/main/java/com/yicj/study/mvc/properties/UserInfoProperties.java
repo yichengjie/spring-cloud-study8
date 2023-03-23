4fba3b5da37f30798e350cc473d5197e3a238d02
yichengjie <626659321@qq.com>
2023-03-23 23:06:01 +0800

    save

--- hello-web-mvc/src/main/java/com/yicj/study/mvc/properties/UserInfoProperties.java
+++ hello-web-mvc/src/main/java/com/yicj/study/mvc/properties/UserInfoProperties.java
@@ -1,10 +1,12 @@
 package com.yicj.study.mvc.properties;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.io.Serializable;
 
+@Slf4j
 @Data
 @ConfigurationProperties("custom.user")
 public class UserInfoProperties implements Serializable {
@@ -12,5 +14,10 @@ public class UserInfoProperties implements Serializable {
   private String username ;
 
   private String address ;
+
+
+  public UserInfoProperties(){
+      log.info("UserInfoProperties new() .");
+  }
 
 }
